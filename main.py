85bbec498e4f6c5cbff7c013611d47f84759cbbf
AnkithAbhayan <ankithabhayan@gmail.com>
2021-04-07 13:36:26 +0100

    added rule command and base json file

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+with open("data.json","r") as JsonFile:
+    data = json.load(JsonFile)
+
+bot = commands.Bot(command_prefix='$')
+bot.remove_command('help')
+
+@bot.command(name='rule')
+async def rule(ctx,*args):
+    index = int(ctx.message.content.split()[1])
+    rule_embed = discord.Embed(title="Rules.",description=data["rules"][index-1],color=0x00cc00)
+    rule_embed.set_footer(text=ctx.guild.name)
+    await ctx.send(embed=rule_embed)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+bot.run(TOKEN)
